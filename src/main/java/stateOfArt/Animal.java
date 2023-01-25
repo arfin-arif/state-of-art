f436501ba151e22b2eca30365c2c064c054501f4
arfin-arif <arifmtbd@gmail.com>
2023-01-25 11:35:31 +0000

    Supper class done

--- src/main/java/stateOfArt/Animal.java
+++ src/main/java/stateOfArt/Animal.java
@@ -14,4 +14,45 @@ public class Animal {
     private int noOfLegs;
     
     
+       Animal(){
+        
+    }
+
+    Animal(boolean vegetarian,String eats, int noOfLegs ) {
+        this.vegetarian=vegetarian;
+        this.eats=eats;
+        this.noOfLegs=noOfLegs;
+      }
+            //  to set is veg or not 
+             public void setVegetarian() {
+                  vegetarian = vegetarian;
+                   }  
+              //  to get is veg or not 
+              public boolean getVegetarian() {
+                   return vegetarian;
+               }
+            //  to set is eats or not 
+             public void setEats(String eats) {
+                   eats = eats;
+                
+               }  
+              //  to get is eats or not 
+             public String getEats() {
+                   return eats;
+               }
+            //  to set is noOfLegs or no
+               public void setnoOfLegs(int noOfLegs) {
+                    if (noOfLegs >=1 && noOfLegs <=4 ) {
+                   noOfLegs = noOfLegs;
+                    } else {
+                    System.out.println("Error! Legs  can't be negative!");
+              }
+               }
+             //  to get is noOfLeg
+               public int getnoOfLegs() {
+                   return noOfLegs;
+               } 
+   
+    
+    
 }
