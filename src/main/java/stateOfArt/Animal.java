b9ab064e89cc2a403f632b0c450e9d791893f93a
arfin-arif <arifmtbd@gmail.com>
2023-01-25 11:34:20 +0000

    taken three variable

--- src/main/java/stateOfArt/Animal.java
+++ src/main/java/stateOfArt/Animal.java
@@ -1,5 +1,17 @@
+/*
+Create a Super class named Animal which has variables (private boolean vegetarian, private
+String eats, private int noOfLegs), a parameterized Constructor, setter methods, getter
+methods for all variables. It also has methods (public boolean isVegetarian(), public void
+setNoOfLegs(int noOfLegs)).
+*/
+
 package stateOfArt;
 
 public class Animal {
+    
+    private boolean vegetarian;
+    private String eats;
+    private int noOfLegs;
+    
     
 }
