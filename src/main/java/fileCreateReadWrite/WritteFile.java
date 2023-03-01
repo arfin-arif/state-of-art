4091c1f0263306ca53a08a36a4d9df197941fbcd
arfin-arif <arifmtbd@gmail.com>
2023-03-01 12:16:01 +0000

    file create,write and read

--- src/main/java/fileCreateReadWrite/WritteFile.java
+++ src/main/java/fileCreateReadWrite/WritteFile.java
@@ -4,13 +4,14 @@ import java.util.Formatter;
 import java.util.Scanner;
 
 public class WritteFile {
+
     public static void main(String[] args) {
-        int id;String name;
-        try {
-            try (Formatter formatter = new Formatter("E:\\Java\\persone\\student.txt")) {
-                Scanner input = new Scanner(System.in);
-                System.out.print("Enter the number of students : ");
-                int num = input.nextInt();
+        int id;
+        String name;
+        try (Formatter formatter = new Formatter("E:\\Java\\persone\\student.txt")) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the number of students : ");
+            int num = input.nextInt();
                 for (int i = 1; i <= num; i++) {
                     System.out.print("Enter Student ID: ");
                     id = input.nextInt();
@@ -18,7 +19,6 @@ public class WritteFile {
                     name = input.next();
                     formatter.format("%d  %s\r\n", id, name);
                 }
-            }
         } catch (Exception e) {
             System.out.println(e);
         }
